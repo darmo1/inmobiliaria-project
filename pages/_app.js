66ad1572cf68dd827bab90ba8c596b1b01e7b1eb
Danilo <darmito10@gmail.com>
2022-01-24 11:36:48 -0500

    add script google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 import Layout from '../Layout'
 
+
+const ID_GOOGLE_ANALYTICS = process.env.ID_GOOGLE_ANALYTICS
+console.log(ID_GOOGLE_ANALYTICS)
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script
+       async
+       src={`https://www.googletagmanager.com/gtag/js?id=${'G-1V2VBQ13Q5'}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1V2VBQ13Q5' );
+          `}    
+      </Script>
        <Component {...pageProps} />
     </Layout>
   )
